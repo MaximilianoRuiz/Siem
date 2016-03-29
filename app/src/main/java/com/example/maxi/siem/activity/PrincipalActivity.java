5692d7a4abd06903c4fd23f3fb06b4d44599b977
Maxi <maxi_ruiz140@hotmail.com>
2016-03-28 22:45:37 -0300

    Loggin and Principal Activities

---
+++ app/src/main/java/com/example/maxi/siem/activity/PrincipalActivity.java
@@ -0,0 +1,19 @@
+package com.example.maxi.siem.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.maxi.siem.R;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
