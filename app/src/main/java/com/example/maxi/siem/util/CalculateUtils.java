adc2162322789a7e830ee39e91ce4ab58749ef9a
Maxi <maxi_ruiz140@hotmail.com>
2016-04-02 00:50:33 -0300

    formater digital hours

--- app/src/main/java/com/example/maxi/siem/util/CalculateUtils.java
+++ app/src/main/java/com/example/maxi/siem/util/CalculateUtils.java
@@ -3,6 +3,7 @@ package com.example.maxi.siem.util;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class CalculateUtils {
@@ -24,25 +25,56 @@ public class CalculateUtils {
 
     public static String getConsumoHoras(String entrada, String salida) {
         dateFormat = new SimpleDateFormat(HH_MM);
-
-        String hora = "00:00";
+        Date dEntrada = null;
+        Date dSalida = null;
+        Calendar primero = Calendar.getInstance();
+        Calendar segundo = Calendar.getInstance();
 
         try {
-            long tiempoEntrada = dateFormat.parse(entrada).getTime();
-            long tiempoSalida = dateFormat.parse(salida).getTime();
+            dEntrada = dateFormat.parse(entrada);
+            dSalida = dateFormat.parse(salida);
+        } catch (ParseException e) {
+            dEntrada = Calendar.getInstance().getTime();
+            dSalida = Calendar.getInstance().getTime();
+        }
 
-            Date d = new Date(tiempoEntrada - tiempoSalida);
+        primero.setTime(dEntrada);
+        segundo.setTime(dSalida);
 
-            hora = dateFormat.format(d);
+        int segHora = segundo.get(Calendar.HOUR);
+        int priHora = primero.get(Calendar.HOUR);
+
+        int segMin = segundo.get(Calendar.MINUTE);
+        int priMin = primero.get(Calendar.MINUTE);
+
+        int hora, min;
+        String tiempoFinal = "00:00";
+
+        try {
+            primero.setTime(dateFormat.parse(entrada));
+            segundo.setTime(dateFormat.parse(salida));
+
+            hora = segHora - priHora;
+
+            if (segMin < priMin) {
+                hora --;
+                min = 60 + segMin - priMin;
+            } else {
+                min = segMin - priMin;
+            }
+
+            tiempoFinal = hora + ":" + min;
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        return hora;
+        return tiempoFinal;
     }
 
     public static String getConsumo(String sHora, String sCosto) {
-        String replace = sHora.replace(":", ".");
+        String horaRedondeada = redondeo(sHora);
+        String replace = horaRedondeada.replace(":", ".");
         double costo = Double.parseDouble(sCosto);
         double hora = Double.parseDouble(replace);
 
@@ -54,5 +86,28 @@ public class CalculateUtils {
         double consumo = Double.parseDouble(Sconsumo);
 
         return Double.toString(saldo - consumo);
+    }
+
+    private static String redondeo(String hora) {
+        Date parse = Calendar.getInstance().getTime();
+        try {
+            parse = dateFormat.parse(hora);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Calendar c = Calendar.getInstance();
+        c.setTime(parse);
+
+        if(c.get(Calendar.MINUTE) > 30) {
+            int incremento = c.get(Calendar.HOUR) + 1;
+            c.set(Calendar.HOUR, incremento);
+        }
+        c.set(Calendar.MINUTE, 0);
+
+        if(c.get(Calendar.HOUR) < 1 && c.get(Calendar.MINUTE) > 30) {
+            c.set(Calendar.HOUR, 1);
+        }
+
+        return dateFormat.format(c.getTime());
     }
 }
--- app/src/main/java/com/example/maxi/siem/util/FormatUtils.java
+++ app/src/main/java/com/example/maxi/siem/util/FormatUtils.java
@@ -8,7 +8,7 @@ import java.util.Calendar;
 public class FormatUtils {
 
     public static final String HH_MM = "hh:mm";
-    public static final String TWO_DECIMALS = "#.##";
+    public static final String TWO_DECIMALS = "##.##";
     private static DateFormat dateFormat;
     private static DecimalFormat decimalFormat;
 
@@ -25,6 +25,6 @@ public class FormatUtils {
     public static String getDoubleFormat (String value) {
         decimalFormat = new DecimalFormat(TWO_DECIMALS);
 
-        return decimalFormat.format(value);
+        return decimalFormat.format(Double.parseDouble(value));
     }
 }
